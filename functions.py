63b7ab3d917ccf7d74851e120a99dcd5a5d780de
kearney <191615342@qq.com>
2023-04-14 20:11:36 +0800

    fix:ctrl and alt not recognize

--- functions.py
+++ functions.py
@@ -27,22 +27,22 @@ def release_all_special_keys():
 
 def activate_special_key_if_pressed(key):
     global is_special_key_pressed
-    if value(key) == 'ctrl_l' or value(key) == 'ctrl_r':
+    if value(key) == 'ctrl_l' or value(key) == 'ctrl_r'  or value(key) == 'ctrl':
         is_special_key_pressed['ctrl'] = True
     elif value(key) == 'shift' or value(key) == 'shift_r':
         is_special_key_pressed['shift'] = True
-    elif value(key) == 'alt_l' or value(key) == 'alt_r':
+    elif value(key) == 'alt_l' or value(key) == 'alt_r' or value(key) == 'alt':
         is_special_key_pressed['alt'] = True
     elif value(key) == 'cmd' or value(key) == 'cmd_l' or value(key) == 'cmd_r':
         is_special_key_pressed['cmd'] = True
 
 def deactivate_special_key_if_released(key):
     global is_special_key_pressed
-    if value(key) == 'ctrl_l' or value(key) == 'ctrl_r':
+    if value(key) == 'ctrl_l' or value(key) == 'ctrl_r'  or value(key) == 'ctrl':
         is_special_key_pressed['ctrl'] = False
     elif value(key) == 'shift' or value(key) == 'shift_r':
         is_special_key_pressed['shift'] = False
-    elif value(key) == 'alt_l' or value(key) == 'alt_r':
+    elif value(key) == 'alt_l' or value(key) == 'alt_r' or value(key) == 'alt':
         is_special_key_pressed['alt'] = False
     elif value(key) == 'cmd' or value(key) == 'cmd_l' or value(key) == 'cmd_r':
         is_special_key_pressed['cmd'] = False
